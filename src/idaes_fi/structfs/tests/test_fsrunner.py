1319d2c580b6f5c2045b1aacf8432b56ca706156
Dan Gunter <dkgunter@lbl.gov>
2026-05-04 10:57:13 -0700

    api notebook

--- src/idaes_fi/structfs/tests/test_fsrunner.py
+++ src/idaes_fi/structfs/tests/test_fsrunner.py
@@ -20,8 +20,6 @@ from .. import fsrunner
 from ..fsrunner import (
     FlowsheetRunner,
     BaseFlowsheetRunner,
-    _find_global_flowsheet,
-    _find_wrapped_main,
     Context,
     run_flowsheet,
 )
@@ -29,7 +27,6 @@ from ..common import ActionNames
 
 from .flash_flowsheet import FS as flash_fs
 import idaes_fi.structfs as structfs
-from idaes.core.util.doctesting import Docstring
 from pyomo.environ import assert_optimal_termination
 
 
@@ -78,64 +75,3 @@ def test_annotation(tmp_path):
     assert runner.model.fs.flash.inlet.flow_mol[0].value == 1
     assert ann["fs.flash._temperature_inlet_ref"]["units"] == "Centipedes"
     assert ann["fs.flash.deltaP"]["is_input"] == False
-
-
-#####
-# Test the code blocks in the structfs/__init__.py
-#####
-
-# pacify linters:
-sfi_before_build_model = sfi_before_set_operating_conditions = sfi_before_init_model = (
-    sfi_before_solve
-) = lambda x: None
-SolverStatus, FS = None, None
-
-#  load the functions from the docstring
-_ds1 = Docstring(structfs.__doc__)
-exec(_ds1.code("before", func_prefix="sfi_before_"))
-exec(_ds1.code("after", func_prefix="sfi_after_"))
-
-
-@pytest.mark.unit
-def test_sfi_before():
-    m = sfi_before_build_model()
-    sfi_before_set_operating_conditions(m)
-    sfi_before_init_model(m)
-    result = sfi_before_solve(m)
-    assert result.solver.status == SolverStatus.ok
-
-
-@pytest.mark.unit
-def test_sfi_after(tmp_path):
-    set_tmp_db(FS, tmp_path)
-
-    FS.run_steps()
-    assert FS.results.solver.status == SolverStatus.ok
-
-
-# pacify linters
-annotate_vars_example = lambda x: None
-# load example function from docstring
-_ds2 = Docstring(BaseFlowsheetRunner.annotate_var.__doc__)
-exec(_ds2.code("annotate_vars"))
-
-
-@pytest.mark.unit
-def test_ann_docs():
-    annotate_vars_example(fr := FlowsheetRunner())
-    ex = fr.annotated_vars["example"]
-    assert ex["fullname"] == "ScalarVar"
-    assert ex["title"] == "Example variable"
-
-
-#####
-# Test utilities to find wrapped functions
-#####
-
-
-@pytest.mark.unit
-def test_find_wrapped():
-    from . import test_simple_wrap
-
-    assert _find_global_flowsheet(test_simple_wrap) == {}
-    assert _find_wrapped_main(test_simple_wrap)
